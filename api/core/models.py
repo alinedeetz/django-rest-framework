90f13674e78c8c62b8128791756bea529334ca6f
Aline Mattos <alinesmattos@hotmail.com>
2021-08-05 00:08:04 -0300

    Add field to .Client

--- api/core/models.py
+++ api/core/models.py
@@ -7,10 +7,10 @@ class Client(models.Model):
     name = models.CharField(max_length=50)
     adress = models.CharField(max_length=50)
     age = models.IntegerField()
-
+    email = models.EmailField(default='default@default.com')
     history = HistoricalRecords()
 
     def __str__(self):
         return self.name
 
-Client.objects.create(name='Anything', adress='This is an andress', age='20')
+# Client.objects.create(name='Anything', adress='This is an andress', age='20')
