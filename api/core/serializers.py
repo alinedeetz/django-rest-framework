e7fd1c0263a51d349fd1078c0cda72e5c388d283
Aline Mattos <alinesmattos@hotmail.com>
2021-08-10 16:46:37 -0300

    Add ProductSerializer class

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import routers, serializers, viewsets
-from .models import Client
+from .models import Client, Product
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ('id', 'name', 'adress', 'age', 'email', 'date')
+        fields = ('id', 'name', 'adress', 'age', 'email')
 
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,5 +13,10 @@ class HistorySerializer(serializers.ModelSerializer):
 
 class DateSerializer(serializers.ModelSerializer):
     pass
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product.history.model 
+        fields = '__all__'
 
 
