fc11f2bee7a67e97063893060a7b2201556c8d4e
Aline Mattos <alinesmattos@hotmail.com>
2021-08-10 16:39:49 -0300

    Add class "Product"

--- api/core/models.py
+++ api/core/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from simple_history.models import HistoricalRecords
 
 # Create your models here.
+class Product(models.Model):
+    product = models.CharField(max_length=50)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    history = HistoricalRecords()
 
 class Client(models.Model):
     name = models.CharField(max_length=50)
     adress = models.CharField(max_length=50)
     age = models.IntegerField()
     email = models.EmailField(default='default@default.com')
-    date = models.DateField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
     history = HistoricalRecords()
 
     def __str__(self):
