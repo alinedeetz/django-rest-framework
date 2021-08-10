d6928fc374c35180e952a66f37d17a42d1658563
Aline Mattos <alinesmattos@hotmail.com>
2021-08-10 16:47:14 -0300

    Add 'products' endpoint

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,16 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from core.views import ClientViewSet, HistoryViewSet
+from core.views import ClientViewSet, ProductViewSet, HistoryViewSet
 
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'clients', ClientViewSet)
-
+router.register(r'products', ProductViewSet)
 router.register(r'history', HistoryViewSet, basename='History')
 
-router.register(r'date', DateViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
